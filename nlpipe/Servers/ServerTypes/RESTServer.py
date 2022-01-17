fbb8366a330e65c52124fb33a25f540c4a42a5bf
Farzam <farzam.fanni@gmail.com>
2022-01-17 17:10:45 +0100

    queuing and tracking logic separated

--- nlpipe/Servers/ServerTypes/RESTServer.py
+++ nlpipe/Servers/ServerTypes/RESTServer.py
@@ -42,7 +42,7 @@ def index():
     return render_template('index.html', **locals())
 
 
-@app_restServer.route('/api/modules/<tool>/', methods=['POST'])
+@app_restServer.route('/api/tools/<tool>/', methods=['POST'])
 @check_auth
 def post_task(tool):
     """
@@ -60,8 +60,9 @@ def post_task(tool):
 
     doc = request.get_data().decode('UTF-8')  # get the document
     task_id = Task.insert({'tool': tool, 'docs': doc, 'status': "PENDING"}).execute()  # adding the task to db
-    doc_id = app_restServer.docStorageModule.\
-        process(tool, doc, doc_id=request.args.get("id"), task_id=task_id)  # stores the doc, and if needed generates the id
+    doc_id = app_restServer.docStorageModule.process(tool,
+                                                     doc, doc_id=request.args.get("id"),  # in case it is given
+                                                     task_id=task_id)  # stores the doc, and if needed generates the id
     resp = Response(doc_id + "\n", status=202)  # create a response object
     resp.headers['Location'] = '/api/tools/{tool}/{doc_id}'.format(**locals())  # endpoint to access doc
     resp.headers['TASK_ID'] = task_id  # add task_id to the response header
@@ -69,81 +70,97 @@ def post_task(tool):
     return resp  # return a response to the use
 
 
-@app_restServer.route('/api/modules/<module>/<id>', methods=['HEAD'])
+@app_restServer.route('/api/tools/<tool>/<id>', methods=['HEAD'])
 @check_auth
-def task_status(module, id):
+def doc_status(tool, id):
     """
-    HEAD gets the status of a task as HTTP Status code.
+    HEAD gets the status of a document as HTTP Status code.
     Response will also contain a status header.
-
-    :param module: The module name
-    :param id: ID of the task to get status for
+    :param tool: The module name
+    :param id: ID of the document to get status for
     """
-    status = app_restServer.docStorageModule.status(module, id)
+    status = app_restServer.docStorageModule.status(tool, id)
     resp = Response(status=STATUS_CODES[status])
     resp.headers['Status'] = status
     return resp
 
 
-@app_restServer.route('/api/modules/<module>/<id>', methods=['GET'])
+@app_restServer.route('/api/tasks/<task_id>', methods=['HEAD'])
 @check_auth
-def result(module, id):
+def task_status(task_id):
+    """
+    HEAD gets the status of a task as HTTP Status code.
+    Response will also contain a status header.
+
+    :param task_id: ID of the task to get status for
+    """
+    status = Task.get(Task.id == task_id).status
+    resp = Response(status=STATUS_CODES[status])
+    resp.headers['Status'] = status
+    return resp
+
+
+@app_restServer.route('/api/tools/<tool>/<doc_id>', methods=['GET'])
+@check_auth
+def result(tool, doc_id):
     """
     GET the processed result of a task.
     If processed OK, returns the result as document with HTTP 200
     If processing failed, returns HTTP 500 with a json document containing the exception
     If task is unknown or not yet processed, will return 404
 
-    :param module: The module name
-    :param id: ID of the task to get result for
+    :param tool: The tool name
+    :param doc_id: ID of the task to get result for
     """
-    format = request.args.get('format', None)
+    return_format = request.args.get('format', None)
     try:
-        result = app_restServer.docStorageModule.result(module, id, format=format)
+        res = app_restServer.docStorageModule.result(tool, doc_id, return_format=return_format)
     except FileNotFoundError:
-        return 'Error: Unknown document: {module}/{id}\n'.format(**locals()), 404
+        return 'Error: Unknown document: {tool}/{id}\n'.format(**locals()), 404
     except Exception as e:
-        result = {"exception_class": type(e).__name__, "message": str(e)}
+        res = {"exception_class": type(e).__name__, "message": str(e)}
         return make_response(jsonify(result), 500)
-    return result, 200
+    return res, 200
 
 
-@app_restServer.route('/api/modules/<module>/', methods=['GET'])
+@app_restServer.route('/api/tools/<tool>/', methods=['GET'])
 @check_auth
-def get_task(module):
+def get_task(tool):
     """
     GET a task to process.
     This is intended to be called by a worker and will set status of the task to STARTED.
     Returns the text to process with HTTP headers ID and Location
 
-    :param module: Module name
+    :param tool: tool name
     """
-    id, doc = app_restServer.docStorageModule.get_task(module)
-    if doc is None:
-        return 'Queue {module} empty!\n'.format(**locals()), 404
-    resp = Response(doc, status=200)
-    resp.headers['Location'] = '/api/modules/{module}/{id}'.format(**locals())
-    resp.headers['ID'] = id
+    doc_id, doc = app_restServer.docStorageModule.get_task(tool)  # get the doc_id and the document
+
+    if doc is None:  # document does not exist
+        return 'Queue {tool} empty!\n'.format(**locals()), 404
+
+    resp = Response(doc, status=200)  # attach the document (unprocessed at this point) to the response
+    resp.headers['Location'] = '/api/tools/{tool}/{doc_id}'.format(**locals())  # endpoint for the document
+    resp.headers['ID'] = doc_id  # document id in the database (queuing system)
     return resp
 
 
-@app_restServer.route('/api/modules/<module>/<id>', methods=['PUT'])
+@app_restServer.route('/api/tools/<tool>/<doc_id>', methods=['PUT'])
 @check_auth
-def put_results(module, id):
+def put_results(tool, doc_id):
     """
     PUT the results of processing.
     If processing failed, use Content-type: prs.error+text and put the error message or diagnostics
-    This is intended to be callsed by a worker and will set the status of the task to DONE or ERROR.
+    This is intended to be called by a worker and will set the status of the task to DONE or ERROR.
 
-    :param module:
-    :param id:
+    :param tool: name of the tool
+    :param doc_id: id of the document
     :return:
     """
     doc = request.get_data().decode('UTF-8')
     if request.content_type == ERROR_MIME:
-        app_restServer.docStorageModule.store_error(module, id, doc)
+        app_restServer.docStorageModule.store_error(tool, doc_id, doc)
     else:
-        app_restServer.docStorageModule.store_result(module, id, doc)
+        app_restServer.docStorageModule.store_result(tool, doc_id, doc)
     return '', 204
 
 
--- nlpipe/Servers/Storage/FileSystemStorage.py
+++ nlpipe/Servers/Storage/FileSystemStorage.py
@@ -16,61 +16,55 @@ class FileSystemStorage(StorageInterface):
     """
 
     def __init__(self, result_dir):
-        self.result_dir = result_dir
-        for module in get_known_tools():
-            logging.debug("checking directory for {result_dir} and module {module}".format(**locals()))
-            self._check_dirs(module.name)
+        self.result_dir = result_dir  # main directory for storing all files
+        for tool in get_known_tools():
+            logging.debug("checking directory for {result_dir} and tool {tool}".format(**locals()))
+            self._check_dirs(tool.name)  # makes a directory for each tool
 
-    def _check_dirs(self, module: str):
-        for subdir in STATUS.values():
-            dirname = os.path.join(self.result_dir, module, subdir)
-            try:
-                os.makedirs(dirname)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
+    def _check_dirs(self, tool: str):
+        try:
+            os.makedirs(os.path.join(self.result_dir, tool))  # directory name per tool
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
-    def _write(self, module, status, id, doc):
-        self._check_dirs(module)
-        fn = self._filename(module, status, id)
-        open(fn, 'w', encoding="UTF-8").write(doc)
+    def _write(self, tool, doc_id, doc):
+        self._check_dirs(tool)  # check if the tool directory exits
+        fn = self._filename(tool, doc_id)  # create the file name with only the doc_id (task tracking by another module)
+        open(fn, 'w', encoding="UTF-8").write(doc)  # write the document
         return fn
 
-    def _read(self, module, status, id):
-        fn = self._filename(module, status, id)
-        return open(fn, encoding="UTF-8").read()
+    def _read(self, tool, doc_id):
+        fn = self._filename(tool, doc_id)  # file name only based on the tool and doc_id
+        return open(fn, encoding="UTF-8").read()  # read the file and return
 
-    def _move(self, module, id, from_status, to_status):
-        fn_from = self._filename(module, from_status, id)
-        fn_to = self._filename(module, to_status, id)
-        os.rename(fn_from, fn_to)
+    def _delete(self, tool, doc_id):
+        fn = self._filename(tool, doc_id)  # file name only based on the tool and doc_id
+        os.remove(fn)  # delete file
 
-    def _delete(self, module, status, id):
-        fn = self._filename(module, status, id)
-        os.remove(fn)
-
-    def _filename(self, module, status, id=None):
-        dirname = os.path.join(self.result_dir, module, STATUS[status])
-        if id is None:
-            return dirname
+    def _filename(self, tool, doc_id=None):
+        tool_directory = os.path.join(self.result_dir, tool)  # separate directory per tool
+        if doc_id is None:
+            return tool_directory  # if no doc_id is given, return the base directory
         else:
-            return os.path.join(dirname, str(id))
+            return os.path.join(tool_directory, str(doc_id))  # if doc_id is given, return full path
 
-    def check(self, module):
-        self._check_dirs(self, module)
-        return module.check_status()
+    def check(self, tool):  # no idea what this is
+        self._check_dirs(self, tool)
+        return tool.check_status()
 
-    def status(self, module, id):
-        for status in STATUS.keys():
-            if os.path.exists(self._filename(module, status, id)):
-                return status
-        return 'UNKNOWN'
+    def status(self, tool, doc_id):
+        try:
+            return Docs.get(Docs.doc_id == doc_id).status  # return the status of a document
+        except Docs.DoesNotExist:
+            return "UNKNOWN"
 
     def process(self, tool, doc, doc_id=None, task_id=None, reset_error=False, reset_pending=False):
         """
         Process the task based on the status of the current document.
         - If "UNKNOWN": it stores the doc using the docStorageModule, and changes the status to "PENDING"
         - if "ERROR" or "STARTED" & reset_pending == TRUE: replaces the previous doc and changes the status to "PENDING"
+
         :param tool: NLP text processing tool (e.g., UPPER_CASE)
         :param doc: document which the NLP task is executed on
         :param doc_id: id of the document
@@ -80,69 +74,95 @@ class FileSystemStorage(StorageInterface):
         :return:å
         """
         if doc_id is None:
-            doc_id = get_id(doc)
-        doc_status = self.status(tool, doc_id)
-        if doc_status == 'UNKNOWN':
+            doc_id = get_id(doc)  # generate a doc_id based on md5 hash
+
+        doc_status = self.status(tool, doc_id)  # get the status of the document. Returns "UNKNOWN" if new
+
+        if doc_status == 'UNKNOWN':  # new document
             logging.debug("Assigning doc {doc_id} to {tool}".format(**locals()))
-            fn = self._write(tool, 'PENDING', doc_id, doc)  # create the file and store the doc
+            fn = self._write(tool, doc_id, doc)  # create the file and store the doc
             Docs.insert({'doc_id': doc_id, 'task_id': task_id,
-                         'path': self._filename(tool, "PENDING"),
+                         'path': self._filename(tool, doc_id),
                          'status': "PENDING"}).execute()  # adding the doc to the db
+
         elif (doc_status == "ERROR" and reset_error) or (doc_status == "STARTED" and reset_pending):
             logging.debug("Re-assigning doc {doc_id} with status {status} to {tool}".format(**locals()))
-            self._delete(tool, doc_status, doc_id)
-            self._write(tool, 'PENDING', doc_id, doc)
             Docs.update({Docs.status: "PENDING"}).\
                 where(Docs.doc_id == doc_id).execute()  # update the status of the doc in db
+
         else:
             logging.debug("Document {doc_id} had status {}".format(self.status(tool, doc_id), **locals()))
+
         return doc_id
 
-    def result(self, module, id, format=None):
-        status = self.status(module, id)
-        if status == 'DONE':
-            result = self._read(module, 'DONE', id)
-            if format is not None:
+    def result(self, tool, doc_id, return_format=None):
+        """
+        Returns the results of processing (via the NLP tool) on the document
+
+        :param tool: specific NLP tool (e.g., TEST_UPPER)
+        :param doc_id: id of the document
+        :param return_format: return format (e.g., json)
+        :return: result of processing the document, converted if indicated
+        """
+        status = self.status(tool, doc_id)  # get the status of the document
+        if status == 'DONE':  # if it is done (i.e., the process has finished)
+            result = self._read(tool, doc_id)  # read the file
+            if return_format is not None:  # for instance can be json
                 try:
-                    result = get_tool(module).convert(id, result, format)
+                    result = get_tool(tool).convert(doc_id, result, return_format)
                 except:
-                    logging.exception("Error converting document {id} to {format}".format(**locals()))
+                    logging.exception("Error converting document {doc_id} to {return_format}".format(**locals()))
                     raise
             return result
         if status == 'ERROR':
-            raise Exception(self._read(module, 'ERROR', id))
-        raise ValueError("Status of {id} is {status}".format(**locals()))
+            raise ValueError("Status of {doc_id} is ERROR")
+        raise ValueError("Status of {doc_id} is {status}".format(**locals()))
 
-    def get_task(self, module):
-        path = self._filename(module, 'PENDING')
-        # I can't find a way to get newest file in python without iterating over all of them
-        # So this seems more robust/faster than looping over python with .getctime for every entry
-        cmd = "ls -rt {path} | head -1".format(**locals())
-        fn = subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
-        if not fn:
-            return None, None  # no files to process
+    def get_task(self, tool):
+        """
+        Returns the next document which has the status=="PENDING" for processing by the worker
+        :param tool: name of the specific NLP tool for processing
+        :return: filename, and document
+        """
         try:
-            self._move(module, fn, 'PENDING', 'STARTED')
-        except FileNotFoundError:
-            # file was removed between choosing it and now, so try again
-            return self.get_task(module)
-        return fn, self._read(module, 'STARTED', fn)
+            doc_id = Docs.get(Docs.status == "PENDING").doc_id  # gets the first document where status is "PENDING"
+            fn = self._filename(tool, doc_id)  # generates the filename for the document
+            if not fn:
+                return None, None  # no files to process
+            Docs.update(status="STARTED").where(Docs.doc_id == doc_id).execute()  # updates the status of the doc
+            return doc_id, self._read(tool, fn)
+        except Docs.DoesNotExist:  # if there are no documents with "PENDING" status
+            return None, None
 
-    def store_result(self, module, id, result):
-        status = self.status(module, id)
-        if status not in ('STARTED', 'DONE', 'ERROR'):
-            raise ValueError("Cannot store result for task {id} with status {status}".format(**locals()))
-        self._write(module, 'DONE', id, result)
-        if status in ('STARTED', 'ERROR'):
-            self._delete(module, status, id)
+    def store_result(self, tool, doc_id, result):
+        """
+        Stores the results of applying the tool to the document
 
-    def store_error(self, module, id, result):
-        status = self.status(module, id)
-        if status not in ('STARTED', 'DONE', 'ERROR'):
-            raise ValueError("Cannot store error for task {id} with status {status}".format(**locals()))
-        self._write(module, 'ERROR', id, result)
-        if status in ('STARTED', 'DONE'):
-            self._delete(module, status, id)
+        :param tool: name of the tool
+        :param doc_id: id of the document
+        :param result: processed text
+        :return: -
+        """
+        status = self.status(tool, doc_id)  # get the current status
+        if status not in ('STARTED', 'DONE', 'ERROR'):  # i don't get this part
+            raise ValueError("Cannot store result for task {doc_id} with status {status}".format(**locals()))
+        self._write(tool, doc_id, result)  # replace the previous document with the new one
+        Docs.update(status="DONE").where(Docs.doc_id == doc_id).execute()  # update the status in the database
+
+    def store_error(self, tool, doc_id, result):
+        """
+        Stores the results of applying the tool to the document, in case there was an error
+
+        :param tool: name of the specific NLP tool
+        :param doc_id: id of the document
+        :param result: result of applying the tool to the document
+        :return: -
+        """
+        status = self.status(tool, doc_id)  # get the current status of the document
+        if status not in ('STARTED', 'DONE', 'ERROR'):  # i don't get this part
+            raise ValueError("Cannot store error for task {doc_id} with status {status}".format(**locals()))
+        self._write(tool, doc_id, result) # replace the previous document with the new one
+        Docs.update(doc_id="ERROR").where(Docs.doc_id == doc_id).execute()  # update the status in the database
 
     def statistics(self, module):
         """Get number of docs for each status for this module"""
